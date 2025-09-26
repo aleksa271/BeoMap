503ec374d5fb0256a8d6313011c159fc125d6adc
Aleksa Perkovic <perkovicaleksa911@gmail.com>
2025-09-26 12:56:08 +0200

    backend v2

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,23 @@
-from flask import Flask, send_from_directory
-from flask_cors import CORS 
-import os 
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/geojson")
+GEOJSON_PATH = os.path.join(os.path.dirname(__file__), "../geojson/beograd_opstine.geojson")
+
+@app.route("/api/geojson")
 def geojson():
-    return send_from_directory(
-        os.path.join(os.path.dirname(__file__), "..", "geojson"),
-        "beograd_opstine.geojson"
-    )
+    return send_file(GEOJSON_PATH, mimetype="application/json")
+
+@app.route("/api/data")
+def data():
+    return jsonify({
+        "Zemun": {"temp": 22, "air": "Dobar"},
+        "Novi Beograd": {"temp": 23, "air": "Umeren"},
+        "Vračar": {"temp": 25, "air": "Loš"}
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
